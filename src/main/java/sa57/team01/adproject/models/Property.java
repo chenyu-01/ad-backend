52a9c3b1c5c052f8d68d4007951f3fe9f4f047da
Zejing Yu <e1285205@u.nus.edu>
2024-01-27 16:51:25 +0800

    modify

--- src/main/java/sa57/team01/adproject/models/Property.java
+++ src/main/java/sa57/team01/adproject/models/Property.java
@@ -4,41 +4,37 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
-@Entity
-@Inheritance (strategy = InheritanceType.TABLE_PER_CLASS)
-public abstract class Property {
+    @Getter
+    @Setter
+    @Entity
+    @Inheritance (strategy = InheritanceType.TABLE_PER_CLASS)
+    public abstract class Property {
 
-    @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private long propertyid;
-    private String town;
-    private PropertyStatus propertyStatus;
-    private int flatType;
-    private String storeyRange;
-    private String streetName;
-    private int floorArea;
+        @Id
+        @GeneratedValue(strategy= GenerationType.IDENTITY)
+        private long propertyid;
+        private String town;
+        @Enumerated(EnumType.STRING)
+        private PropertyStatus propertyStatus;
+        private int flatType;
+        private String storeyRange;
+        private String streetName;
+        private int floorArea;
 
-    @OneToOne(mappedBy = "property")
-    private Appointment appointment;
+        @OneToOne(mappedBy = "property")
+        private Appointment appointment;
 
-    public Property(){
-        super();
-    };
+        public Property(){
+            super();
+        }
 
-    public Property (long propertyid, String town, PropertyStatus propertyStatus,int flatType,String storeyRange, String streetName,int floorArea){
-      this.propertyid = propertyid;
-      this.town = town;
-      this.propertyStatus = propertyStatus;
-      this.flatType=flatType;
-      this.storeyRange=storeyRange;
-      this.streetName=streetName;
-      this.floorArea=floorArea;
+        public Property (long propertyid, String town, PropertyStatus propertyStatus,int flatType,String storeyRange, String streetName,int floorArea){
+          this.propertyid = propertyid;
+          this.town = town;
+          this.propertyStatus = propertyStatus;
+          this.flatType=flatType;
+          this.storeyRange=storeyRange;
+          this.streetName=streetName;
+          this.floorArea=floorArea;
+        }
     }
-
-
-
-
-
-}
--- src/main/java/sa57/team01/adproject/models/PropertyStatus.java
+++ src/main/java/sa57/team01/adproject/models/PropertyStatus.java
@@ -1,5 +1,7 @@
 package sa57.team01.adproject.models;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 
 public enum PropertyStatus {
     rented,soldOut,forRent,forSale;
