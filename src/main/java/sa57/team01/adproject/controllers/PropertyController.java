a31e09710defa262182fedf39cfb0299d8624f02
chenyu-01 <chenyu101@outlook.com>
2024-01-30 11:42:18 +0800

    create Controllers

---
+++ src/main/java/sa57/team01/adproject/controllers/PropertyController.java
@@ -0,0 +1,10 @@
+package sa57.team01.adproject.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/property")
+public class PropertyController {
+
+
+
+}
