c09abe19403a8169a92376fa0da573ff3e7adea3
preethivenkat5 <preethivenkat5@gmail.com>
2024-01-30 23:51:04 +0800

    made property list corrected

--- src/main/java/sa57/team01/adproject/controllers/PropertyController.java
+++ src/main/java/sa57/team01/adproject/controllers/PropertyController.java
@@ -1,6 +1,7 @@
 package sa57.team01.adproject.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,22 +9,30 @@ import org.springframework.web.bind.annotation.RestController;
 import sa57.team01.adproject.DTO.PropertyDTO;
 import sa57.team01.adproject.DTO.RentalPropertyDTO;
 import sa57.team01.adproject.DTO.SalePropertyDTO;
+import sa57.team01.adproject.models.Property;
 import sa57.team01.adproject.models.RentalProperty;
 import sa57.team01.adproject.models.SaleProperty;
+import sa57.team01.adproject.repositories.PropertyReposity;
+import sa57.team01.adproject.services.PropertyService;
 import sa57.team01.adproject.services.RentalPropertyService;
 import sa57.team01.adproject.services.SalePropertyService;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @RestController("/api/property")
 public class PropertyController {
 
     private final RentalPropertyService rentalPropertyService;
     private final SalePropertyService salePropertyService;
+
+    private final PropertyService propertyService;
     @Autowired
-    public PropertyController(RentalPropertyService rentalPropertyService, SalePropertyService salePropertyService) {
+    public PropertyController(RentalPropertyService rentalPropertyService, SalePropertyService salePropertyService,PropertyService propertyService) {
         this.rentalPropertyService = rentalPropertyService;
         this.salePropertyService = salePropertyService;
+        this.propertyService=propertyService;
     }
     @GetMapping("/rentlist/{page}")
     public ResponseEntity<?> getAllRentProperties(@PathVariable int page) {
@@ -41,5 +50,69 @@ public class PropertyController {
         List<SalePropertyDTO> salePropertyDTOS = SalePropertyDTO.from(saleProperties);
         return ResponseEntity.ok(salePropertyDTOS);
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<Property> getPropertyById(@PathVariable Long id){
+        Optional<Property> propertyOptional = propertyService.getPropertyById(id);
+        return propertyOptional
+                .map(property -> ResponseEntity.ok(property))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/listbydate")
+    public ResponseEntity<List<Property>> getListByDate(Date date){
+        List<Property>properties=propertyService.getListByDate(date);
+        if(properties.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(properties,HttpStatus.OK);
+    }
+
+    @GetMapping("/listbyprice")
+    public ResponseEntity<List<Property>> getListByPrice(double price){
+        List<Property>properties=propertyService.getListByPrice(price);
+        if(properties.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(properties,HttpStatus.OK);
+    }
+    @GetMapping("/listbytown")
+    public ResponseEntity<List<Property>> getListByTown(String town){
+        List<Property>properties=propertyService.getListByTown(town);
+        if(properties.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(properties,HttpStatus.OK);
+    }
+
+    @GetMapping("/listbyfloorarea")
+    public ResponseEntity<List<Property>> getListByFloorarea(int floorarea){
+        List<Property>properties=propertyService.getListByFloorArea(floorarea);
+        if(properties.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(properties,HttpStatus.OK);
+    }
+
+    @GetMapping("/listbystorey")
+    public ResponseEntity<List<Property>> getListByStorey(String storey){
+        List<Property>properties=propertyService.getListByStoreyRange(storey);
+        if(properties.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(properties,HttpStatus.OK);
+
+    }
+
+    @GetMapping("/listbystreet")
+    public ResponseEntity<List<Property>> getListByStreet(String street){
+        List<Property>properties=propertyService.getListByStreet(street);
+
+        if(properties.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(properties,HttpStatus.OK);
+    }
+
+
 
 }
