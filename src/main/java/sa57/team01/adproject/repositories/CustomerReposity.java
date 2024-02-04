75e30c5e1c1ebcee655d70adef945d8c6ba7adca
preethivenkat5 <preethivenkat5@gmail.com>
2024-02-04 12:36:49 +0800

    added login functionality

---
+++ src/main/java/sa57/team01/adproject/repositories/CustomerReposity.java
@@ -0,0 +1,13 @@
+package sa57.team01.adproject.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import sa57.team01.adproject.models.Customer;
+
+@Repository
+public interface CustomerReposity extends JpaRepository<Customer,Long> {
+
+    @Query("Select c from Customer c where c.email=:email")
+    Customer findByEmail(String email);
+}
---
+++ src/main/java/sa57/team01/adproject/services/CustomerServiceImpl.java
@@ -0,0 +1,23 @@
+package sa57.team01.adproject.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import sa57.team01.adproject.models.Customer;
+import sa57.team01.adproject.repositories.CustomerReposity;
+
+@Service
+public class CustomerServiceImpl implements CustomerService{
+    @Autowired
+    public CustomerReposity customerReposity;
+
+
+    @Override
+    public Customer findByEmail(String email) {
+        return customerReposity.findByEmail(email);
+    }
+
+    @Override
+    public Customer save(Customer customer) {
+        return customerReposity.save(customer);
+    }
+}
